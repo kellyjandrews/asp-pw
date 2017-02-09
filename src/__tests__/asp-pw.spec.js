a72a6b74b3e123dd13ecd0f33d018fb247731947
Kelly J Andrews <kelly@kellyjandrews.com>
2017-02-09 10:09:45 -0500

    init

---
+++ src/__tests__/asp-pw.spec.js
@@ -0,0 +1,8 @@
+// developeraccount123
+// AGEBXjISlvRiBbuTxweXlWDIIoPYES2iUneI+Q3PyzS4HKuffyTSv2ZK3ayeCW7v/A==
+// 11112222
+// ANpvKHlv2PUXYlw+/QEe9YezO6DecDUN26yBGV/u4rAHd89gY2atI55f2RhIHBOSjw==
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+})
