fcc16a61a78a286c19e395cd41eee459c7863a50
Kelly J Andrews <kelly@kellyjandrews.com>
2017-02-09 12:32:00 -0500

    v2 build out for validation and hashing

--- src/__tests__/asp-pw.spec.js
+++ src/__tests__/asp-pw.spec.js
@@ -1,8 +1,32 @@
-// developeraccount123
-// AGEBXjISlvRiBbuTxweXlWDIIoPYES2iUneI+Q3PyzS4HKuffyTSv2ZK3ayeCW7v/A==
-// 11112222
-// ANpvKHlv2PUXYlw+/QEe9YezO6DecDUN26yBGV/u4rAHd89gY2atI55f2RhIHBOSjw==
+import password from '../asp-pw.js';
 
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
-})
+const test1 = 'developeraccount123';
+const hash1 = 'AGEBXjISlvRiBbuTxweXlWDIIoPYES2iUneI+Q3PyzS4HKuffyTSv2ZK3ayeCW7v/A==';
+
+const test2 = '11112222';
+const hash2 = 'ANpvKHlv2PUXYlw+/QEe9YezO6DecDUN26yBGV/u4rAHd89gY2atI55f2RhIHBOSjw==';
+
+const test3 = 'Password@1';
+
+describe('Validation', () => {
+  it('Throws errors when invalid input', () => {
+    expect(() => {password.validate('only one input');}).toThrow();
+    expect(() => {password.validate(test1, 'invalid hash');}).toThrow();
+  });
+
+  it('validates v2 compatible passwords', () => {
+    expect(password.validate(test1, hash1)).toBe(true);
+    expect(password.validate(test2, hash2)).toBe(true);
+  });
+});
+
+describe('Hashing', () => {
+  it('Throws errors when invalid input', () => {
+    expect(() => {password.hash();}).toThrow();
+  });
+  it('Hashes password that validates', () => {
+    const hash3 = password.hash(test3);
+    expect(hash3).toBeTruthy();
+    expect(password.validate(test3, hash3)).toBe(true);
+  });
+});
--- src/asp-pw.js
+++ src/asp-pw.js
@@ -1,1 +1,63 @@
 import crypto from 'crypto';
+
+
+/* =======================
+ * HASHED PASSWORD FORMATS
+ * =======================
+ *
+ * Version 2:
+ * PBKDF2 with HMAC-SHA1, 128-bit salt, 256-bit subkey, 1000 iterations.
+ * (See also: SDL crypto guidelines v5.1, Part III)
+ * Format: { 0x00, salt, subkey }
+ *
+ * Version 3:
+ * PBKDF2 with HMAC-SHA256, 128-bit salt, 256-bit subkey, 10000 iterations.
+ * Format: { 0x01, prf (UInt32), iter count (UInt32), salt length (UInt32), salt, subkey }
+ * (All UInt32s are stored big-endian.)
+ */
+
+
+function validate(pw, hashPw) {
+  if (!pw || !hashPw) throw 'Missing password or hash input.';
+
+  const hashSrc = parseHash(hashPw);
+  if (hashSrc.length != 49) throw 'Invalid hash.';
+
+  // TODO add logic for v2/v3 switch here (or something else?)
+  const testPw = hash(pw, hashSrc.salt);
+  return testPw === hashPw;
+}
+
+function parseHash(hashPw) {
+  const src = new Buffer(hashPw, 'base64');
+  const v = Buffer.alloc(1);
+  const salt = Buffer.alloc(16);
+  const bytes = Buffer.alloc(32);
+  src.copy(v,0,0,1);
+  src.copy(salt,0,1,17);
+  src.copy(bytes,0,17,49);
+  return {
+    v: v,
+    salt: salt,
+    bytes: bytes,
+    length: src.length
+  };
+}
+
+function hash(pw, salt) {
+  if (!pw && !salt) throw 'No valid input.';
+
+  const output = Buffer.alloc(49);
+  const saltSrc = salt || crypto.randomBytes(16);
+  const bytes = crypto.pbkdf2Sync(pw, saltSrc, 1000, 32, 'sha1');
+  output.fill(0);
+  saltSrc.copy(output, 1, 0, 16);
+  bytes.copy(output, 17, 0, 32);
+
+  return output.toString('base64');
+}
+
+export default {
+  validate: validate,
+  hash:hash
+};
